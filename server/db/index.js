092b2d2444b8d4e5547275a289b05d1d80525683
davilux <mx.davilux@gmail.com>
2023-02-09 17:49:44 -0500

    Completed initial db setup.

---
+++ server/db/index.js
@@ -0,0 +1,9 @@
+const db = require('./db')
+const Product = require('./models/Product')
+
+module.exports = {
+  db,
+  models: {
+    Product
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App"></div>
+    <div className="App">
+      < Navbar />
+    </div>
   );
 }
 
