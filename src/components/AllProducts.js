154cbeafa9f1fca3570f9381cc73404747f0a249
davilux <mx.davilux@gmail.com>
2023-02-10 13:32:16 -0500

    Basic redux setup. Successfully retrieving a row from the database and displaying it to the client.

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProducts } from '../redux/reducers/productsSlice'
+import { v4 as uuidv4 } from 'uuid';
 
 const AllProducts = () => {
+  const dispatch = useDispatch();
+  const allProducts = useSelector((state) => state.products.allProducts)
+  const status = useSelector((state) => state.products.status)
+
+  useEffect(() => {
+    if (status !== 'succeeded') {
+      dispatch(getAllProducts())
+    }
+  }, [status, dispatch])
+
   return (
-    <h1>
-      All products
-    </h1>
+    <>
+      <h1>
+        All products
+      </h1>
+      <ul className="container">
+        {allProducts.map((product) => (
+          <div className="card" key={uuidv4()}>
+            {product.name}
+          </div>
+        ))}
+      </ul>
+    </>
   )
 }
 
